28c06c6adaded990f8219aface1beace40335123
Edith-Samuel Sbenghe <samuel.sbenghe@gmail.com>
2024-05-03 21:36:32 +0100

    small change

--- 3110. Score of a String/main.go
+++ 3110. Score of a String/main.go
@@ -1,5 +1,4 @@
 // You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters. Return the score of s.
-
 package main
 
 import (
